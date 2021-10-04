b18fb3b9e3299b7f9a490d4ca3967498bab4449b
Peter Kraus <lungdrache@yahoo.de>
2021-10-04 13:26:48 +0200

    Projektdateien hinzufügen.

---
+++ SteamGamesExporter/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteamGamesExporter
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // open Json File
+            OpenFileExplorer("JSON-File (*.json)|*.json");
+
+
+            MessageBox.Show("worked", "File Content at path: ", MessageBoxButtons.OK);
+        }
+
+        static string OpenFileExplorer(string filter,string startFolder = "c:\\")
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = startFolder;
+                openFileDialog.Filter = filter;
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                while (openFileDialog.ShowDialog() != DialogResult.OK || string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                {
+
+                    if (!string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                    {
+
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName;
+
+                        //Read the contents of the file into a stream
+                        var fileStream = openFileDialog.OpenFile();
+
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                        if (openFileDialog.ShowDialog() != DialogResult.OK)
+                        {
+                            DialogResult result = MessageBox.Show("Want to try again?", "Wrong File", MessageBoxButtons.RetryCancel);
+                            switch (result)
+                            {
+                                case DialogResult.Cancel:
+                                    Environment.Exit(0);
+                                    break;
+                                case DialogResult.Retry:
+
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+
+                        DialogResult result = MessageBox.Show("Want to Exit?", "Error", MessageBoxButtons.YesNo);
+                        switch (result)
+                        {
+                            case DialogResult.Yes:
+                                Environment.Exit(0);
+                                break;
+                            case DialogResult.No:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                return fileContent;
+            }
+        }
+        
+    }
+}
